24b51fad694c545c4b825b5d26ca940b44fb37b8
Muhammad-Abid11 <abidkhan11845@gmail.com>
2024-02-22 17:11:30 +0500

    Changes For Multiple Inputs , Some Changes in API

--- src/app/[id]/Show.jsx
+++ src/app/[id]/Show.jsx
@@ -9,11 +9,13 @@ export default function Show({ singleProduct }) {
     const pass = process.env.NEXT_PUBLIC_SECRET_UPASS;
     // console.log("Hi user and pass", user, pass)
 
-    const [textOne, setTextOne] = useState('');
-    const [textTwo, setTextTwo] = useState('');
+    // const [textOne, setTextOne] = useState('');
+    // const [textTwo, setTextTwo] = useState('');
     const [downloadDisabled, setDownloadDisabled] = useState(true);
     const [updateMeme, setUpdateMeme] = useState("")
+    const [boxText, setBoxText] = useState({});
 
+    /* for 2 inputs 
     const handleTextOneChange = (event) => {
         setTextOne(event.target.value);
     };
@@ -21,26 +23,61 @@ export default function Show({ singleProduct }) {
     const handleTextTwoChange = (event) => {
         setTextTwo(event.target.value);
     };
+     */
+
+    const handleText = (e) => {
+        // console.log(e.target.name, e.target.value)
+        const { name, value } = e.target;
+        setBoxText(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    }
 
     const handleGenerateMeme = async () => {
-        if (textOne && textTwo) {
-            // console.log('Generating meme with Text One:', textOne, 'and Text Two:', textTwo);
-            // const postAPI = `https://api.imgflip.com/caption_image?template_id=${singleProduct.id}&username=${user}&password=${pass}&text0=${textOne}&text1=${textTwo}`
-            const postAPI = `https://api.imgflip.com/caption_image?template_id=${singleProduct.id}&username=AbidKhan4&password=imgFlip786@&text0=${textOne}&text1=${textTwo}`
-            const res = await fetch(postAPI)
-            const result = await res.json()
-            // console.log("result--->", result)
-            setUpdateMeme(result.data.url)
-            setTextOne("");
-            setTextTwo("");
 
-            setDownloadDisabled(false);
-
-
-        } else {
-            // One or both input fields are empty, show alert
-            alert('Please fill both input fields.');
+        // console.log(Object.values(boxText), Object.values(boxText).length)
+        const filledBoxes = Object.values(boxText).filter(text => text.trim() !== '');
+        if (filledBoxes.length !== singleProduct.box_count) {
+            alert('Please fill all input boxes.');
+            return;
         }
+
+
+        // console.log('boxtext_0', boxText.text_0)
+        // console.log('boxtext_1', boxText.text_1)
+        // console.log('boxtext_2', boxText.text_2)
+        // console.log('boxtext_3', boxText.text_3)
+        const postAPI = `https://api.imgflip.com/caption_image?template_id=${singleProduct.id}&username=AbidKhan4&password=imgFlip786@&boxes[0][text]=${boxText.text_0 == undefined ? "" : boxText.text_0}&boxes[1][text]=${boxText.text_1 == undefined ? "" : boxText.text_1}&boxes[2][text]=${boxText.text_2 == undefined ? "" : boxText.text_2}&boxes[3][text]=${boxText.text_3 == undefined ? "" : boxText.text_3}`
+
+        const res = await fetch(postAPI)
+        const result = await res.json()
+        // console.log("result--->", result)
+        setUpdateMeme(result.data.url)
+        setBoxText("")
+        setDownloadDisabled(false);
+
+
+        /*  for 2 inputs
+               if (textOne && textTwo) {
+                    // console.log('Generating meme with Text One:', textOne, 'and Text Two:', textTwo);
+                    // const postAPI = `https://api.imgflip.com/caption_image?template_id=${singleProduct.id}&username=${user}&password=${pass}&text0=${textOne}&text1=${textTwo}`
+                    const postAPI = `https://api.imgflip.com/caption_image?template_id=${singleProduct.id}&username=AbidKhan4&password=imgFlip786@&text0=${textOne}&text1=${textTwo}`
+                    const res = await fetch(postAPI)
+                    const result = await res.json()
+                    // console.log("result--->", result)
+                    setUpdateMeme(result.data.url)
+                    setTextOne("");
+                    setTextTwo("");
+        
+                    setDownloadDisabled(false);
+        
+        
+                } else {
+                    // One or both input fields are empty, show alert
+                    alert('Please fill both input fields.');
+                } 
+            */
     };
 
     const handleDownloadMeme = async () => {
@@ -98,26 +135,18 @@ export default function Show({ singleProduct }) {
                             <div class="lg:w-1/2 bg-slate-200 sm:w-1/2 m-auto px-4 ">
                                 <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">{singleProduct.name}</h2>
 
-                                <input
+                                {[...Array(singleProduct.box_count)].map((data, index) => <input //similarly yhn hamen input box ki lenght malum thi is liye utne input return by map
                                     required
+
                                     type="text"
-                                    name="text-one"
-                                    placeholder='Enter Text One'
+                                    name={`text_${index}`}
+                                    placeholder={`Enter Text Box ${index + 1}`}
                                     className='p-2 rounded'
-                                    value={textOne}
-                                    onChange={handleTextOneChange}
+                                    value={boxText[`text_${index}`] || ''}//boxText k state k object me jao or whn text_0,1,2,3 ko clear krne k liye
+                                    onChange={handleText}
+                                    style={{ display: "block", margin: "auto", marginBottom: "10px" }}
                                 />
-                                <br />
-                                <br />
-                                <input
-                                    required
-                                    type="text"
-                                    name="text-two"
-                                    placeholder='Enter Text Two'
-                                    className='p-2 rounded'
-                                    value={textTwo}
-                                    onChange={handleTextTwoChange}
-                                />
+                                )}
                                 <button
                                     className="flex mx-auto my-8  text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded"
                                     onClick={handleGenerateMeme}
